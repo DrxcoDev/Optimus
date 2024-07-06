eb14e558625f57af13d7d8ef07820d45acfb9c11
DrxcoDev <drxcovalorant@gmail.com>
2024-07-06 17:01:56 +0200

    first commit

---
+++ src/core.js
@@ -0,0 +1,60 @@
+/*
+    @ Optimus Framework JavaScript
+
+*/
+
+/* 
+
+// DOM Ready Event
+// Load fast
+*/
+
+class OptimizedFramework {
+  constructor(options) {
+    this.options = options;
+    this.state = {};
+    this.init();
+  }
+
+  init() {
+    try {
+      this.state = this.options.state || {};
+      this.render();
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  setState(newState) {
+    try {
+      this.state = { ...this.state, ...newState };
+      this.render();
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  async render() {
+    try {
+      const root = document.querySelector(this.options.el);
+      const content = await this.options.template(this.state);
+      root.innerHTML = content;
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  handleError(error) {
+    const root = document.querySelector(this.options.el);
+    root.innerHTML = (
+      '<div style="color: red; padding: 10px; border: 2px solid red; background-color: #ffe6e6;">' +
+      '<h2>Se ha producido un error:</h2>' +
+      '<p>' + error.message + '</p>' +
+      '<pre>' + error.stack + '</pre>' +
+      '</div>'
+    );
+    console.error('Error:', error);
+  }
+}
+
+  
---
+++ test/app.js
@@ -0,0 +1,24 @@
+
+
+/*Example of how use app.js*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    async function loadTemplate(state) {
+      // Simula un retraso para el ejemplo de carga diferida
+      return (
+        '<div>' +
+          '<h1> Hola Mundo </h1>' +
+          '<button onclick="window.app.setState({ message: \'¡Hola, JavaScript optimizado!\' })">Cambiar mensaje</button>' +
+        '</div>'
+      );
+    }
+  
+    window.app = new OptimizedFramework({
+      el: '#app', /* BODY */
+      state: {
+        message: 'Hola, Mundo optimizado!' /* YOUR CLASSES*/
+      },
+      template: loadTemplate /* THE MAIN FUNCTION TO START */
+    });
+  });
+  
