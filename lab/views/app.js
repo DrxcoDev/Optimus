2d38826b4e4ca86b9f046a7e5c851a86f9c27cf5
DrxcoDev <drxcovalorant@gmail.com>
2024-07-11 14:08:30 +0200

    IGNORE

--- lab/views/app.js
+++ lab/views/app.js
@@ -1,20 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-    async function loadTemplate(state) {
-      // Simula un retraso para el ejemplo de carga diferida
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      return (
-        '<div>' +
-          '<h1>Hello Optimus</h1>' +
-        '</div>'
-      );
-    }
-  
-    window.app = new Optimus({
-      el: '#app',
-      state: {
-        message: 'Hola, Mundo optimizado!'
-      },
-      template: loadTemplate
-    });
-  });
-  
+const pug = require('pug');
+const fs = require('fs');
+
+// Compilar el archivo Pug a HTML
+const html = pug.renderFile('./index.pug');
+
+// Escribir el resultado en un archivo HTML
+fs.writeFileSync('index.html', html);
+
+console.log('Archivo HTML generado con éxito.');
